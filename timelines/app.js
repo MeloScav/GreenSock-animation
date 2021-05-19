2f8eb9233c5edc0b502bfcafd7f35f939cae427f
Méloé <meloscavezzoni@gmail.com>
2021-05-19 17:05:26 +0200

    First timeline

--- timelines/app.js
+++ timelines/app.js
@@ -7,3 +7,15 @@ const iceCreamImage = document.querySelector(
 );
 const title = document.querySelector(".title");
 const txt = document.querySelector(".txt");
+
+const TL = gsap.timeline();
+
+// TL.to(catImage, { opacity: 1, y: 0 })
+//   .to(mountainImage, { opacity: 1, y: 0 })
+//   .to(iceCreamImage, { opacity: 1, y: 0 })
+//   .to(title, { opacity: 1, y: 0 });
+
+TL.to(catImage, { autoAlpha: 1, y: 0 })
+  .to(mountainImage, { autoAlpha: 1, y: 0 })
+  .to(iceCreamImage, { autoAlpha: 1, y: 0 })
+  .to(title, { autoAlpha: 1, y: 0 });

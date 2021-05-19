e27cac76eb23d5cf4dcaa1900173e7ccc6d08e73
Méloé <meloscavezzoni@gmail.com>
2021-05-19 16:02:36 +0200

    Selected items

---
+++ timelines/app.js
@@ -0,0 +1,9 @@
+const catImage = document.querySelector(".container-images img:nth-child(1)");
+const mountainImage = document.querySelector(
+  ".container-images img:nth-child(2)"
+);
+const iceCreamImage = document.querySelector(
+  ".container-images img:nth-child(3)"
+);
+const title = document.querySelector(".title");
+const txt = document.querySelector(".txt");

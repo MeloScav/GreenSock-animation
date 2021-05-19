f7141378d0c5070ca2e624b217ac869acac6ff4f
Méloé <meloscavezzoni@gmail.com>
2021-05-19 11:33:16 +0200

    Easings

--- the-basics/app.js
+++ the-basics/app.js
@@ -1,28 +1,46 @@
 const catImage = document.querySelector(".container-images img:nth-child(1)");
 const btnCat = document.querySelector(".cat-fade");
 
+/* .to() METHOD */
 // gsap.to(catImage, { x: 100, y: 100, scale: 2, duration: 2 });
 
 // gsap.to("h1", { color: "red", backgroundColor: "blue", duration: 1 });
 
+/* .from() METHOD */
 // gsap.from(catImage, { opacity: 0, duration: 3 });
 
 // btnCat.addEventListener("click", () => {
 //     gsap.from(catImage, { opacity: 0, duration: 3 });
 //   });
 
+/* .fromTo() METHOD */
 // btnCat.addEventListener("click", () => {
 //   gsap.fromTo(catImage, { opacity: 0 }, { opacity: 1, duration: 3 });
 // });
 
-btnCat.addEventListener("click", () => {
-  gsap.to(catImage, {
-    x: 100,
-    y: 100,
-    scale: 2,
-    duration: 2,
-    delay: 0.2,
-    repeat: 1,
-    repeatDelay: 2,
-  });
+/* REPEAT AND DELAY */
+// btnCat.addEventListener("click", () => {
+//   gsap.to(catImage, {
+//     x: 100,
+//     y: 100,
+//     scale: 2,
+//     duration: 2,
+//     delay: 0.2,
+//     repeat: 1,
+//     repeatDelay: 2,
+//   });
+// });
+
+/* EASINGS */
+// gsap.to(".container-images img", {
+//   y: 300,
+//   scale: 1.2,
+//   duration: 2,
+//   ease: "power1.in",
+// });
+gsap.to(".container-images img", {
+  y: 300,
+  scale: 1.2,
+  duration: 2,
+  ease: "bounce.out",
 });

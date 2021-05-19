3d6cf1c668fe8826c311da8c99464c5e3aee9083
Méloé <meloscavezzoni@gmail.com>
2021-05-19 10:11:53 +0200

    .from() method

--- the-basics/app.js
+++ the-basics/app.js
@@ -1,5 +1,12 @@
 const catImage = document.querySelector(".container-images img:nth-child(1)");
+const btnCat = document.querySelector(".cat-fade");
 
-gsap.to(catImage, { x: 100, y: 100, scale: 2, duration: 2 });
+// gsap.to(catImage, { x: 100, y: 100, scale: 2, duration: 2 });
 
-gsap.to("h1", { color: "red", backgroundColor: "blue", duration: 1 });
+// gsap.to("h1", { color: "red", backgroundColor: "blue", duration: 1 });
+
+// gsap.from(catImage, { opacity: 0, duration: 3 });
+
+btnCat.addEventListener("click", () => {
+  gsap.from(catImage, { opacity: 0, duration: 3 });
+});

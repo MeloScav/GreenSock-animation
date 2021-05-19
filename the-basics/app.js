5e0336f85002e62187f74064145ecfbc7438e280
Méloé <meloscavezzoni@gmail.com>
2021-05-19 12:31:03 +0200

    Random

--- the-basics/app.js
+++ the-basics/app.js
@@ -76,4 +76,13 @@ const btnCat = document.querySelector(".cat-fade");
 //   },
 // });
 
-gsap.to(".container-btns button", { duration: 0.2, x: 50, stagger: 0.2 });
+// gsap.to(".container-btns button", { duration: 0.2, x: 50, stagger: 0.2 });
+
+/* RANDOM */
+// gsap.to(".btn", { duration: 0.2, x: "random(-100, 100)", stagger: 0.1 });
+// gsap.to(".btn", { duration: 0.2, x: "random(-100, 100, 10)", stagger: 0.1 });
+gsap.to(".btn", {
+  duration: 0.2,
+  x: "random([50, 30, -30, 80])",
+  stagger: 0.1,
+});

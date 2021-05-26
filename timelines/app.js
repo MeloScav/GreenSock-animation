58368fdeb7d9880c2e88bc1958c15297421404a8
Méloé <meloscavezzoni@gmail.com>
2021-05-26 10:53:58 +0200

    Utility parameters

--- timelines/app.js
+++ timelines/app.js
@@ -8,15 +8,36 @@ const iceCreamImage = document.querySelector(
 const title = document.querySelector(".title");
 const txt = document.querySelector(".txt");
 
-const TL = gsap.timeline();
+// const TL = gsap.timeline();
 
 // TL.to(catImage, { opacity: 1, y: 0 })
 //   .to(mountainImage, { opacity: 1, y: 0 })
 //   .to(iceCreamImage, { opacity: 1, y: 0 })
 //   .to(title, { opacity: 1, y: 0 });
 
-TL.to(catImage, { autoAlpha: 1, y: 0, duration: 1 }, 1)
-  .to(mountainImage, { autoAlpha: 1, y: 0, duration: 1 }, 0)
-  .to(iceCreamImage, { autoAlpha: 1, y: 0, duration: 1 }, ">");
+// TL.to(catImage, { autoAlpha: 1, y: 0, duration: 1 }, 1)
+//   .to(mountainImage, { autoAlpha: 1, y: 0, duration: 1 }, 0)
+//   .to(iceCreamImage, { autoAlpha: 1, y: 0, duration: 1 }, ">");
 // .to(title, { autoAlpha: 1, y: 0, duration: 1 }, "-=0.75")
 // .to(txt, { autoAlpha: 1, y: 0, duration: 1 }, "-=0.75");
+
+const TL = gsap.timeline({
+  defaults: {
+    duration: 1,
+    ease: "power4",
+  },
+  // repeat: -1,
+  // repeatDelay: 0.5,
+  // yoyo: true,
+  paused: true,
+});
+
+TL.to(catImage, { autoAlpha: 1, y: 0 })
+  .to(mountainImage, { autoAlpha: 1, y: 0 }, "-=0.75")
+  .to(iceCreamImage, { autoAlpha: 1, y: 0 }, "-=0.75")
+  .to(title, { autoAlpha: 1, y: 0 }, "-=0.75")
+  .to(txt, { autoAlpha: 1, y: 0 }, "-=0.75");
+
+setTimeout(() => {
+  TL.play();
+}, 1000);

8cfd424dab7a98b4d08f1769fd64114b49ede197
Méloé <meloscavezzoni@gmail.com>
2021-05-26 16:27:34 +0200

    Selected items

---
+++ animated-home-page/app.js
@@ -0,0 +1,6 @@
+const navLinks = document.querySelectorAll("nav a");
+const navImages = document.querySelectorAll("nav img");
+const title = document.querySelector("h1");
+const verticalLine = document.querySelector(".middle-line");
+const grappesLogo = document.querySelector(".home-content img");
+const homeBtn = document.querySelector(".home-content button");

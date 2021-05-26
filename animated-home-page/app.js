539cff20bbd51bfe06c71bf896b9b530722f9a5c
Méloé <meloscavezzoni@gmail.com>
2021-05-26 16:28:09 +0200

    Animation of elements

--- animated-home-page/app.js
+++ animated-home-page/app.js
@@ -4,3 +4,14 @@ const title = document.querySelector("h1");
 const verticalLine = document.querySelector(".middle-line");
 const grappesLogo = document.querySelector(".home-content img");
 const homeBtn = document.querySelector(".home-content button");
+
+window.addEventListener("load", () => {
+  const TLFADE = gsap.timeline();
+
+  TLFADE.to(title, { autoAlpha: 1, y: 0, delay: 0.2 })
+    .to(verticalLine, { height: 170 }, "-=0.2")
+    .to(grappesLogo, { autoAlpha: 1, y: 0 }, "-=0.2")
+    .to(homeBtn, { autoAlpha: 1, y: 0 }, "-=0.2")
+    .to(navLinks, { autoAlpha: 1, y: 0, duration: 0.4, stagger: 0.1 }, "-=0.2")
+    .to(navImages, { autoAlpha: 1, y: 0 });
+});

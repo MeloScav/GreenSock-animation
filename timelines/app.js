7e1667b136b1bfc5a1c4b85a2a257fce70bb2c5c
Méloé <meloscavezzoni@gmail.com>
2021-05-26 10:06:06 +0200

    Text animation + duration added

--- timelines/app.js
+++ timelines/app.js
@@ -15,7 +15,8 @@ const TL = gsap.timeline();
 //   .to(iceCreamImage, { opacity: 1, y: 0 })
 //   .to(title, { opacity: 1, y: 0 });
 
-TL.to(catImage, { autoAlpha: 1, y: 0 })
-  .to(mountainImage, { autoAlpha: 1, y: 0 })
-  .to(iceCreamImage, { autoAlpha: 1, y: 0 })
-  .to(title, { autoAlpha: 1, y: 0 });
+TL.to(catImage, { autoAlpha: 1, y: 0, duration: 1 })
+  .to(mountainImage, { autoAlpha: 1, y: 0, duration: 1 })
+  .to(iceCreamImage, { autoAlpha: 1, y: 0, duration: 1 })
+  .to(title, { autoAlpha: 1, y: 0, duration: 1 })
+  .to(txt, { autoAlpha: 1, y: 0, duration: 1 });

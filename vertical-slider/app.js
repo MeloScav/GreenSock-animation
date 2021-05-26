23ac2babf7d8f8ef51706c91fe42cec309a64775
Méloé <meloscavezzoni@gmail.com>
2021-05-26 19:38:38 +0200

    Countdown animation

---
+++ vertical-slider/app.js
@@ -0,0 +1,9 @@
+const init = () => {
+  const animation = gsap.timeline({ repeat: -1 });
+
+  animation
+    .to(".vertical-slider p", { y: 0, autoAlpha: 1, stagger: 1 })
+    .to(".vertical-slider p", { y: -100, autoAlpha: 0, stagger: 1 }, 1);
+};
+
+window.addEventListener("load", init);

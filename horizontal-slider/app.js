4e965c90cda61e27cf7db052d1b351256f29654f
Méloé <meloscavezzoni@gmail.com>
2021-05-27 10:18:36 +0200

    Left button animation and end animation + refactoring

--- horizontal-slider/app.js
+++ horizontal-slider/app.js
@@ -15,19 +15,51 @@ const animRight = () => {
   });
 };
 
-btnRight.addEventListener("click", () => {
-  if (index === 2) {
-    gsap.to(container, {
-      keyframes: [
-        { duration: 0.1, x: -4 },
-        { duration: 0.1, x: 4 },
-        { duration: 0.1, x: -4 },
-        { duration: 0.1, x: 0 },
-      ],
-    });
-    return;
-  }
+const animLeft = () => {
+  const TLLeft = gsap.timeline();
 
-  index++;
-  animRight();
+  TLLeft.set(indexIndic, { innerText: index }).to(allSlides[index], {
+    duration: 0.6,
+    x: "-100%",
+  });
+};
+
+const negation = gsap.to(container, {
+  paused: true,
+  keyframes: [
+    { duration: 0.1, x: -4 },
+    { duration: 0.1, x: 4 },
+    { duration: 0.1, x: -4 },
+    { duration: 0.1, x: 0 },
+  ],
+});
+
+const handleDirection = (direction) => {
+  if (direction === "next") {
+    if (index === 2) {
+      negation.seek(0);
+      negation.play();
+      return;
+    }
+
+    index++;
+    animRight();
+  } else if (direction === "prev") {
+    if (index === 0) {
+      negation.seek(0);
+      negation.play();
+      return;
+    }
+
+    animLeft();
+    index--;
+  }
+};
+
+btnRight.addEventListener("click", () => {
+  handleDirection("next");
+});
+
+btnLeft.addEventListener("click", () => {
+  handleDirection("prev");
 });

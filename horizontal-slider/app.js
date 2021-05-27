5f1f13a84e3d4923014865d287ef5e174dac3ef1
Méloé <meloscavezzoni@gmail.com>
2021-05-27 08:50:35 +0200

    Right button animation

--- horizontal-slider/app.js
+++ horizontal-slider/app.js
@@ -5,3 +5,14 @@ const allSlides = Array.from(document.querySelectorAll(".slide"));
 const indexIndic = document.querySelector(".counter span:nth-child(1)");
 
 let index = 0;
+
+const animRight = () => {
+  const TLRight = gsap.timeline();
+
+  TLRight.to(allSlides[index], { duration: 0.6, x: 0 });
+};
+
+btnRight.addEventListener("click", () => {
+  index++;
+  animRight();
+});
